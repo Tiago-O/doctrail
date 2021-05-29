c28fb7c4e17f501ce0ce6cc2def6418b1bc22153
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-29 11:30:07 +0100

    user validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :repos
+  has_many :docs, through: :repos
+  has_many :versions
+  has_many :comments
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :address, presence: true, length: { minimum: 2 }
 end
