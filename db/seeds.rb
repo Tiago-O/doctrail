35f4a0a5c4cb63734bbaba9633ddaac262b9906e
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-29 17:12:11 +0100

    some seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Clean the database
+puts 'Deleting users, userdocs, docs, versions & commments ...'
+Comment.destroy_all
+Version.destroy_all
+Doc.destroy_all
+User.destroy_all
+Userdoc.destroy_all
+
+# Users, Docs & Userdocs
+puts 'Creating users, docs & userdocs ...'
+
+4.times do
+  user = User.create(first_name: 'Tiago',last_name: 'Oliveirinha', address: 'Arroios', phone_number: '911231212', company: 'Covillage', email: 'tiago@me.pt', password: '123456')
+  doc = Doc.create(title: Faker::Lorem.sentence, text: Faker::Lorem.paragraphs(number: 20))
+  Userdoc.create(owner: true, user, doc)
+end
+4.times do
+  user = User.create(first_name: 'Diogo',last_name: 'Brôco', address: 'Intendente', phone_number: '934567890', company: 'Pipedrive', email: 'diogo@me.pt', password: '123456')
+  doc = Doc.create(title: Faker::Lorem.sentence, text: Faker::Lorem.paragraphs(number: 20))
+  Userdoc.create(owner: true, user, doc)
+end
+4.times do
+  user = User.create(first_name: 'Felipe',last_name: 'Santos', address: 'Alvalade', phone_number: '969876543', company: 'The Best', email: 'felipe@me.pt', password: '123456')
+  doc = Doc.create(title: Faker::Lorem.sentence, text: Faker::Lorem.paragraphs(number: 20), locked: false, final: false)
+  Userdoc.create(owner: true, user, doc)
+end
+
+puts "Created #{User.count} users"
+puts "Created #{Doc.count} docs"
+puts "Created #{Userdoc.count} userdocs"
+
+# Versions
+puts 'Creating versions (texts identical to docs) ...'
+Doc.all.each do |doc|
+  version.create(text: doc.text, accepted: false, doc: doc, user: doc.user)
+end
+
+puts "Created #{Version.count} versions"
+
+# Comments
+puts 'Creating comments ...'
+
+Version.all.each do |version|
+  Comment.create(text: Faker::Lorem.sentence, version: version, user: version.user)
+end
+
+puts "Created #{Comment.count} comments"
