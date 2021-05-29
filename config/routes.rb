fbc3c92d2268b941463bddf9f778cb7136438b7d
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-29 12:45:54 +0100

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
+
+  resources :users, except: [:index, :new, :create]
+
+  resources :docs do
+    resources :versions, only: [:create] do
+      resources :comments, only: [:create]
+    end
+  end
+
+  resources :versions, except: [:create, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
