088d764af1ec06c9d5ee7609df7ee396d34d98c3
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-05-30 22:32:04 +0100

    seeds users with photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 # Clean the database
 puts 'Deleting users, userdocs, docs, versions & commments ...'
 Comment.destroy_all
@@ -9,18 +11,25 @@ Userdoc.destroy_all
 # Users, Docs & Userdocs
 puts 'Creating users, docs & userdocs ...'
 
-4.times do
-  user = User.create(first_name: 'Tiago',last_name: 'Oliveirinha', address: 'Arroios', phone_number: '911231212', company: 'Covillage', email: 'tiago@me.pt', password: '123456')
-  doc = Doc.create(title: Faker::Lorem.sentence, text: Faker::Lorem.paragraphs(number: 20))
-  Userdoc.create(owner: true, user, doc)
-end
-4.times do
-  user = User.create(first_name: 'Diogo',last_name: 'Brôco', address: 'Intendente', phone_number: '934567890', company: 'Pipedrive', email: 'diogo@me.pt', password: '123456')
-  doc = Doc.create(title: Faker::Lorem.sentence, text: Faker::Lorem.paragraphs(number: 20))
-  Userdoc.create(owner: true, user, doc)
-end
-4.times do
-  user = User.create(first_name: 'Felipe',last_name: 'Santos', address: 'Alvalade', phone_number: '969876543', company: 'The Best', email: 'felipe@me.pt', password: '123456')
+user = User.create(first_name: 'Tiago',last_name: 'Oliveirinha', address: 'Arroios', phone_number: '911231212', company: 'Covillage', email: 'tiago@me.pt', password: '123456')
+file = URI.open('https://avatars.githubusercontent.com/u/63858592?v=4')
+user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+user.save
+4.times { create_doc(user) }
+
+user = User.create(first_name: 'Diogo',last_name: 'Brôco', address: 'Intendente', phone_number: '934567890', company: 'Pipedrive', email: 'diogo@me.pt', password: '123456')
+file = URI.open('https://avatars.githubusercontent.com/u/76879710?v=4')
+user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+user.save
+4.times { create_doc(user) }
+
+user = User.create(first_name: 'Felipe',last_name: 'Santos', address: 'Alvalade', phone_number: '969876543', company: 'The Best', email: 'felipe@me.pt', password: '123456')
+file = URI.open('https://avatars.githubusercontent.com/u/75455106?v=4')
+user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+user.save
+4.times { create_doc(user) }
+
+def create_doc(user)
   doc = Doc.create(title: Faker::Lorem.sentence, text: Faker::Lorem.paragraphs(number: 20), locked: false, final: false)
   Userdoc.create(owner: true, user, doc)
 end
