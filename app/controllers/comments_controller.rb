98cd8fad6cdb5ff358ffb6b1e8fb9008df4dcb59
Diogo Broco <dpintobroco@gmail.com>
2021-06-01 19:22:20 +0100

    Added edit update and destroy methods and before action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[edit update destroy]
+
   def create
     @comment = Comment.new(comments_params)
     @doc = Doc.find(params[:id])
@@ -9,13 +11,29 @@ class CommentsController < ApplicationController
     if comment.save
       redirect_to @doc
     else
-      render :new # should be 'docs/show'
+      render 'doc/show'
     end
+  end
+
+  def edit; end
+
+  def update
+    @comment.update(comments_params)
+    redirect_to version_path(current_user)
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to version_path(current_user)
   end
 
   private
 
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comments_params
-    params.require(:comment).permit(:text)
+    params.require(:comment).permit(:text, :user_id, :version_id)
   end
 end
