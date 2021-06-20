eb2fc99f88b87f3cd4691e0ddf18d7663095e166
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-06-20 15:57:16 +0100

    now 1st version is the original version

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,6 +36,6 @@ import { diff_match } from '../components/diff_match';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  loadDynamicBannerText();
+  // loadDynamicBannerText();
   diff_match();
 });
